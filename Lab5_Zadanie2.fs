08bc86da8dd896b64a92229147c3e1dff9a659e6
Dahaka3122 <116275646+Dahaka3122@users.noreply.github.com>
2024-11-19 16:26:36 +0100

    F# Lab 5 Zadanie 2

---
+++ Lab5_Zadanie2.fs
@@ -0,0 +1,21 @@
+﻿type Tree<'T> =
+    | Empty
+    | Node of 'T * Tree<'T> * Tree<'T>
+
+let rec findRecursive value tree =
+    match tree with
+    | Empty -> false
+    | Node(v, left, right) ->
+        if v = value then true
+        else findRecursive value left || findRecursive value right
+
+let findIterative value tree =
+    let rec loop stack =
+        match stack with
+        | [] -> false
+        | Empty :: rest -> loop rest
+        | Node(v, left, right) :: rest ->
+            if v = value then true
+            else loop (left :: right :: rest)
+    loop [tree]
+
