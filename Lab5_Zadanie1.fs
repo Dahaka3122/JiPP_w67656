51cf404dcc5f688e45d5db25c84c3fcfdf11ad1c
Dahaka3122 <116275646+Dahaka3122@users.noreply.github.com>
2024-11-19 16:25:59 +0100

    F# Lab 5 Zadanie 1

---
+++ Lab5_Zadanie1.fs
@@ -0,0 +1,16 @@
+﻿let rec fib n =
+    if n <=1 then n
+    else fib(n-1) + fib(n-2)
+
+let wynik = fib 4
+printf "rek fib z 4: %d" wynik
+
+let fibTail n =
+    let rec aux n a b =
+        match n with
+        | 0 -> a
+        | _ -> aux (n - 1) b (a + b)
+    aux n 0 1
+
+let wynik1 = fibTail 5
+printf "rek fibTail z 5: %d" wynik1
